ea4f6a03f2a69b7f97fc66da88f0a51e511391f8
michel <1024836+michel-lacle@users.noreply.github.com>
2020-12-10 07:47:32 -0500

    add entry point examples

--- src/merkle_example/__main__.py
+++ src/merkle_example/__main__.py
@@ -1,5 +1,28 @@
+import sys
+
+
+def usage():
+    print("")
+    print("\tUsage: merkle_example run")
+    print("")
+    print("\tDescription: This is an example main entry point")
+    print("")
+
+
 def my_main():
-    print("executing main file")
+    print("executing main entry point for merkle_example")
+
+    print(f"You have provided {len(sys.argv)} argument(s): ")
+    print("")
+    for i, arg in enumerate(sys.argv):
+        print(f"Argument {i:>6}: {arg}")
+
+    if len(sys.argv) <= 1:
+        usage()
+        exit(-1)
+    else:
+        print("running the merkle_example")
+        exit(0)
 
 
 if __name__ == "__main__":
