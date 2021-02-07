bb58ba75cdea37b3c37bb474c194e936699d6d1e
Michel Lacle <mlacle@progressrail.com>
2021-02-06 19:44:26 -0500

    first version of example python project

--- publish.sh
+++ publish.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -e
+
 rm -fr build
 rm -fr dist
 
 python3 -m pip install --upgrade setuptools wheel
 
 python3 setup.py sdist bdist_wheel
-# publish to our AWS PyPi repo
-#python3 -m twine upload --repository codeartifact dist/*
 
 python3 -m  pip install twine
+
+twine check dist/*
+
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
--- setup.py
+++ setup.py
@@ -9,10 +9,16 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 install_reqs = [x.strip() for x in all_reqs if 'git+' not in x]
 dep_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' in x]
 
+# read the contents of your README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
-    name="python_project_template",
+    name="miminal_example_project",
     version="1.0.0",
-    description="provides the capability to generate features",
+    description="a minimalist python project template",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(where='src', exclude=['python', 'tests', 'infrastructure']),
     include_package_data=True,
     install_requires=install_reqs,
