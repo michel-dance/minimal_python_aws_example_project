80aca324acc5ac039cbc61924f9fa63b7393e4b5
michel <1024836+michel-lacle@users.noreply.github.com>
2021-02-08 10:30:41 -0500

    add ability to publish to code artifact

--- publish_codeartifact.sh
+++ publish_codeartifact.sh
@@ -5,12 +5,12 @@ set -e
 rm -fr build
 rm -fr dist
 
-aws codeartifact login --tool pip --repository private --domain f1kart --domain-owner 472521221391 --profile cto
+aws codeartifact login --tool pip --repository private --domain f1kart --domain-owner ${AWS_ACCOUNT} --profile ${AWS_PROFILE}
 python3 -m pip install --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 python3 -m  pip install twine
 
-aws codeartifact login --tool twine --repository private --domain f1kart --domain-owner 472521221391 --profile cto
+aws codeartifact login --tool twine --repository private --domain f1kart --domain-owner ${AWS_ACCOUNT} --profile ${AWS_PROFILE}
 
 twine check dist/*
 twine upload --repository codeartifact dist/*
--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="minimal_aws_example_project",
-    version="1.0.3",
+    version="1.0.4",
     author='Michel Lacle',
     author_email='michel+pypi@f1kart.com',
-    url='https://github.com/michel-lacle/minimal_example_project',
+    url='https://github.com/michel-lacle/minimal_aws_example_project',
     description="a minimal Python project example with unit testing, command line interface, PyPi publishing capabilities",
     long_description=long_description,
     long_description_content_type='text/markdown',
