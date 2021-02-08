00b3f8f638efabc062182dc57039245a4fdeee76
michel <1024836+michel-lacle@users.noreply.github.com>
2021-02-08 16:57:21 -0500

    add scaffolding for submitting batch job

--- src/example/__main__.py
+++ src/example/__main__.py
@@ -1,23 +1,26 @@
-import sys
 from .example2 import example_tree
 from .example1 import print_s3_buckets
+from .example1 import submit_batch_job
 import argparse
 
 
-def my_main():
+def main():
 
     parser = argparse.ArgumentParser(description='Example AWS Python project with many benefits.')
 
     parser.add_argument(
         '--calculate-merkle-hash',
-        nargs = 4,
+        nargs=4,
         metavar=('string1', 'string2', 'string3', 'string4'),
         help='compute the merkle hash of input strings')
     parser.add_argument(
         '--print-s3-buckets',
         action='store_true',
-        help='show the s3 buckets in this account'
-    )
+        help='show the s3 buckets in this account')
+    parser.add_argument(
+        '--submit-batch-job',
+        action='store_true',
+        help='submit a job to AWS for batch processing')
     args = parser.parse_args()
 
     if args.calculate_merkle_hash:
@@ -29,10 +32,12 @@ def my_main():
             args.calculate_merkle_hash[3]))
     if args.print_s3_buckets:
         print_s3_buckets()
+    if args.submit_batch_job:
+        submit_batch_job()
     else:
         parser.print_help()
         exit(-1)
 
 
 if __name__ == "__main__":
-    my_main()
+    main()
