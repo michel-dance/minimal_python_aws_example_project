972ca382499c60d203facc5ff5526ac52d5e8018
michel <1024836+michel-lacle@users.noreply.github.com>
2021-02-09 11:23:07 -0500

    adding ecs execution role

--- infrastructure/batch/role.tf
+++ infrastructure/batch/role.tf
@@ -1,3 +1,4 @@
+# this gives the container permission to access AWS resources
 resource "aws_iam_role" "container" {
 
   name = "${var.name}-container"
@@ -37,4 +38,36 @@ resource "aws_iam_role_policy" "container" {
   role = aws_iam_role.container.name
 
   policy = data.aws_iam_policy_document.container.json
+}
+
+# this gives the ecs agents to execute the container task
+# and download the Docker images from ECR
+resource "aws_iam_role" "ecs" {
+
+  name = "${var.name}-ecs"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-ec2-container-registry" {
+  role       = aws_iam_role.ecs.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution" {
+  role       = aws_iam_role.ecs.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
